78be3c068185c95fdc3d8d770356c4fe0f669420
JiangZhenxiang <1036615730@qq.com>
2019-10-18 20:46:36 +0800

    Week8 Homework

--- float-equal.c
+++ float-equal.c
@@ -1,15 +1,15 @@
 //实现代码判断2个float变量相等
 //误差精确到时小数点后5位
 #include <stdio.h>
-#include <math.h>
-int main(){
-    float float1,float2;
-    printf("Please enter two floats:");
-    scanf("%f,%f",&float1,&float2);
-    if (fabs(float1-float2)<0.00001){
-        printf("These two floats are equal");
+int main()
+{
+    float m = 1.78;
+    float x = 1.779999, y = 2;
+    float z = m / y, z1 = x / y;
+    if (z-z1<0.00001) {
+        z = (m + x) / y;
     }
-    else{
-        printf("These two floats are not equal.");
-    }
+    printf("Z is %f\n", z);
+    printf("Z1 is %f\n", z1);
+    return 0;
 }
