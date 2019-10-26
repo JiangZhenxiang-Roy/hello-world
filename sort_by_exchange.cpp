0a5db7f8458aaf9c9545d396121b3aa61c30c754
JiangZhenxiang <1036615730@qq.com>
2019-10-26 17:32:23 +0800

    Change

--- sort_by_exchange.cpp
+++ sort_by_exchange.cpp
@@ -3,38 +3,60 @@
 #include <stdlib.h>
 int main() {
     //Collect numbers
-    int n, i, *x;
+    int n, i, *x, m, temp;
     int init = 5;
     x = (int *) malloc(init * sizeof(int));
     printf("Please enter the number of numbers:");
     scanf("%d", &n);
     printf("Please enter numbers in row (one number each line)\n");
     for (i = 0; i < n; i++) {
-        printf("No.%d:", i+1);
+        printf("No.%d:", i + 1);
         if (i < init)
             scanf("%d", x + i);
         else {
-            x=(int*) realloc(x,(i+1)* sizeof(int));
+            x = (int *) realloc(x, (i + 1) * sizeof(int));
             scanf("%d", x + i);
         }
     }
 
-    //Sort numbers
-    int j,temp;
-    for (i = 0 ;i<n;i++){
-        for (j=i+1;j<n;j++){
-            if (x[i]>x[j]){
-                temp=x[i];
-                x[i]=x[j];
-                x[j]=temp;
+    //Sort numbers by exchange
+    for (m=n;m>0;m--) {
+        for (i = 0; i < m-1; i++) {
+            if (x[i] > x[i + 1]) {
+                temp = x[i];
+                x[i] = x[i + 1];
+                x[i + 1] = temp;
             }
         }
     }
-    for (i=0;i<j;i++)
+
+    //Show results
+    for (i=0;i<n;i++){
         printf("%d ",x[i]);
-    return 0;
+    }
 }
 
+
+
+
+//Sort numbers(Another way)
+//    int j,temp;
+//    for (i = 0 ;i<n;i++){
+//        for (j=i+1;j<n;j++){
+//            if (x[i]>x[j]){
+//                temp=x[i];
+//                x[i]=x[j];
+//                x[j]=temp;
+//            }
+//        }
+//    }
+//    for (i=0;i<j;i++)
+//        printf("%d ",x[i]);
+//    return 0;
+//}
+
+
+//Teacher:
 //  int x[3] = {5, 2, 1}, i;
 //  while(1) {
 //    scanf("%d %d %d", &x[0], &x[1], &x[2]);
